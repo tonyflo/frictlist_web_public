fd53507297f415da467355c7b848e28c4df9b7a3
Tony Florida <raddfood@gmail.com>
2014-03-28 08:58:26 -0700

    Accomodating db redesign where there's a 1 to many relationship from user to mate and mate to frict

--- scripts/remove_frict.php
+++ scripts/remove_frict.php
@@ -2,18 +2,16 @@
 /* @file update_frict.php
  * @date 2014-03-22
  * @author Tony Florida
- * @brief Allows a user to update a Frict from their list
+ * @brief Allows a user to remove a frict from their list
  */
    include "../../frictlist_private/credentials.php";
    include "../../frictlist_private/account.php";
-   //TODO
-   $table_user="user";
+
    $table_frict="frict";
    
-   $uid=$_POST["uid"];
    $frict_id=$_POST["frict_id"];
    
-   echo remove_frict($uid, $frict_id, $db, $table_user, $table_frict);
+   echo remove_frict($frict_id, $db, $table_frict);
 
    $db->close();
 ?>
---
+++ scripts/remove_mate.php
@@ -0,0 +1,18 @@
+<?php
+/* @file remove_mate.php
+ * @date 2014-03-27
+ * @author Tony Florida
+ * @brief Allows a user to remove a mate from their list
+ */
+   include "../../frictlist_private/credentials.php";
+   include "../../frictlist_private/account.php";
+ 
+   $table_mate="mate";
+   $table_frict="frict";
+   
+   $mate_id=$_POST["mate_id"];
+   
+   echo remove_mate($mate_id, $db, $table_mate, $table_frict);
+
+   $db->close();
+?>
