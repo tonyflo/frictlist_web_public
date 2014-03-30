f9fc6d6c8e0d62babd0decea6bec0be5f06f290d
Tony Florida <raddfood@gmail.com>
2014-03-29 20:45:45 -0700

    Added function to send table of outgoing requests to app. '

--- scripts/get_outgoing_status.php
+++ scripts/get_outgoing_status.php
@@ -1,5 +1,5 @@
 <?php
-/* @file add_mate.php
+/* @file get_outgoing_status.php
  * @date 2014-03-29
  * @author Tony Florida
  * @brief Ability to search for a mate
@@ -8,11 +8,8 @@
    include "../../frictlist_private/account.php";
    
    $uid=$_POST["uid"];
-   $firstname=$_POST["firstname"];
-   $lastname=$_POST["lastname"];
-   $gender=$_POST["gender"];
   
-   echo search_mate($uid, $firstname, $lastname, $gender, $db);
+   echo get_outgoing_status($uid, $db);
 
    $db->close();
 ?>
